87e20ae3ceb23ba4665b6234352f89f118459dad
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-10 16:02:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
   def initialize(name, breed="Mutt")
     @name = name
+    @breed = breed
   end
   def name=(name)
     @name = name
@@ -8,4 +9,9 @@ class Dog
   def name
     @name
   end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
 end
